c0379a02521444309e20074b36832a0fde0afb01
Dmitry Verteyko <verteyko1990@gmail.com>
2021-01-30 02:19:44 +0300

    added component tags

--- frontend/src/pages/homePage.vue
+++ frontend/src/pages/homePage.vue
@@ -3,6 +3,17 @@
     <h1 class="app__title">
       HOME
     </h1>
+    <div>
+      <h2 class="app__subtitle">Tags</h2>
+      <div class="app-home__tags">
+        <div class="app-home__tags-item"
+             @click="selectTagArticle(tag)"
+             v-for="tag in uniqueTags"
+             :key="tag">
+          #{{ tag }}
+        </div>
+      </div>
+    </div>
     <loader v-if="isLoader"/>
     <postsList v-if="allArticles.length" />
     <div class="app-home__placeholder" v-else>
@@ -26,6 +37,7 @@ export default {
   },
   setup() {
     const allArticles = ref([]);
+    const uniqueTags = ref([]);
     const isLoader = ref(false);
 
     provide('allArticles', allArticles);
@@ -35,15 +47,31 @@ export default {
       postApi.getAllArticles().then((resp) => {
         isLoader.value = false;
         allArticles.value = sortBy(resp.data, (el) => el.created_at).reverse();
+        const tagsList = [];
+        resp.data.forEach((el) => {
+          el.tags.forEach((tag) => {
+            tagsList.push(tag);
+          });
+        });
+        const unique = [...new Set(tagsList.map((item) => item))];
+        uniqueTags.value = unique;
       }).catch((e) => {
         isLoader.value = false;
         console.error(e);
       });
     });
 
+    const selectTagArticle = ((tag) => {
+      console.log('tag', tag);
+      const test = allArticles.value.filter((el) => el.tags.forEach((item) => item === tag));
+      console.log('test', test);
+    });
+
     return {
       allArticles,
       isLoader,
+      uniqueTags,
+      selectTagArticle,
     };
   },
 };
@@ -56,6 +84,25 @@ export default {
 
   &__placeholder {
     margin-top: 20px;
+  }
+
+  &__tags {
+    display: flex;
+    max-width: 400px;
+    padding: 10px;
+    flex-wrap: wrap;
+    border-top: 1px solid $color-logo;
+
+    &-item {
+      padding: 10px;
+      color: $color-logo;
+      cursor: pointer;
+      transition: color .15s ease-in;
+
+      &:hover {
+        color: $color-black;
+      }
+    }
   }
 }
 
